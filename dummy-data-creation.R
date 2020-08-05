09b805d621cd49182ca3d42f7cd85035af9dbd3c
Alasdair Sykes <alasdair.sykes@gmail.com>
2020-08-05 14:24:11 +0100

    Adjusted calves distribution for Craig B

--- dummy-data-creation.R
+++ dummy-data-creation.R
@@ -4,14 +4,14 @@ library(mc2d)
 
 # dummy data for app
 n_sample <- 50
+set.seed(2605)
 
 # actual kpi (only one that's not fake) -- calves reared per 100 cows
-calf_rear_pc <- rpert(n = n_sample, mode = 87, min = 72, max = 110, shape = 30)
+calf_rear_pc <- rpert(n = n_sample, mode = 84, min = 72, max = 110, shape = 30)
 
 # add in min and max to make it realistic
 calf_rear_pc[1:2] <- c(72, 101)
 
-set.seed(2605)
 dummy_data <- tibble(`Calves reared per 100 cows` = calf_rear_pc,
                      kpi2 = rlnorm(n_sample, meanlog = log(10), sdlog = log(2)),
                      kpi3 = runif(n_sample, min = 2, max = 11),
